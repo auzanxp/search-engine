625d8d0600b2c2ab20c594a30247177daced704f
Auzan <istikhoriauzan@gmail.com>
2023-06-09 21:24:50 +0700

    feat: search page

---
+++ src/components/SearchComp.vue
@@ -0,0 +1,68 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            searchTerm: '',
+            searchResults: [],
+            searchPerformed: false,
+        };
+    },
+    methods: {
+        searchCountries() {
+            if (this.searchTerm === '') {
+                this.searchResults = [];
+                return;
+            }
+
+            axios
+                .get(`https://restcountries.com/v3.1/name/${this.searchTerm}`)
+                .then((response) => {
+                    this.searchResults = response.data;
+                    this.searchPerformed = true;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+        performSearch() {
+            if (this.searchTerm === '') {
+                this.searchResults = [];
+                return;
+            }
+
+            axios
+                .get(`https://restcountries.com/v3.1/name/${this.searchTerm}`)
+                .then((response) => {
+                    this.searchResults = response.data;
+                    this.searchPerformed = true;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+        getCountryDetails(country) {
+            axios
+                .get(`https://restcountries.com/v3.1/name/${country.name.common}`)
+                .then((response) => {
+                    const countryId = response.data[0].cca2;
+                    this.$router.push({ name: 'desc-country', params: { country: countryId } });
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+        getCountryLink(country) {
+            return {
+                name: 'desc-country',
+                params: {
+                    country: country.cca2,
+                },
+            };
+        },
+    },
+};
+</script>
+
+<template></template>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="search-wrap">
+        <h1>This is an Search View</h1>
+    </div>
+</template>
+  
+<style>
+@media (min-width: 1024px) {
+    .search-wrap {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
